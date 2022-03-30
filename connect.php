f51d43a5ec04a294ff49e1a19653fa0f248bd3bd
pickle40 <tanotodavidriski@yahoo.co.id>
2022-03-30 10:46:47 +0700

    v1

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$link = mysqli_connect("localhost", "david", "kucingitulucu", "cobacoba");
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
+?>
