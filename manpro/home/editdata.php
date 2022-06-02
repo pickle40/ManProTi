21f479eae4e2687e5ce3bd8452b218714f6d73d7
moonwolf666 <94817358+moonwolf666@users.noreply.github.com>
2022-06-02 20:22:03 +0700

    Delete manpro directory

--- manpro/home/editdata.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once '../connect.php';
-	$id=$_REQUEST['id'];
-	echo $id;
-	mysqli_query($link, "UPDATE FROM `pegawai` WHERE `pid`='$_REQUEST[id]'") or die(mysqli_error());
-	header("location:listpegawai.php");
-?>
